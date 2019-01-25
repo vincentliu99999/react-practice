3c36366456a5f8b9bc2ee4f0b4398c165d2b626e
vincentliu99999 <vincentliu99999@gmail.com>
2019-01-25 16:24:57 +0800

    extract components with keys

--- src/index.js
+++ src/index.js
@@ -19,18 +19,26 @@ const About = () => (
   </div>
 );
 
-function NumberList(props) {
+function ListItem(props) {
+    // Correct! There is no need to specify the key here:
+    return <li>{props.value}</li>;
+  }
+  
+  function NumberList(props) {
     const numbers = props.numbers;
     const listItems = numbers.map((number) =>
-      <li key={number.toString()}>
-        {number}
-      </li>
+      // Correct! Key should be specified inside the array.
+      <ListItem key={number.toString()}
+                value={number} />
+  
     );
     return (
-      <ul>{listItems}</ul>
+      <ul>
+        {listItems}
+      </ul>
     );
   }
-
+  
   const numbers = [1, 2, 3, 4, 5];
 
 // ReactDOM.render(<App />, document.getElementById('root'));
