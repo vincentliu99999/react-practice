9e7ebb089f8e9db4d4517ef7568b80cd2ccabef2
vincentliu99999 <vincentliu99999@gmail.com>
2019-01-25 13:04:09 +0800

    add button toggle event

--- src/HelloTo.js
+++ src/HelloTo.js
@@ -2,16 +2,22 @@ import React from "react";
 
 class HelloTo extends React.Component {
   constructor(props) {
-    console.dir(props);
+    console.dir('props', props);
     super(props);
     console.log('constructor');
     this.handleClick = this.handleClick.bind(this);
-    this.state = {date: new Date()};
+    this.state = {
+        date: new Date(),
+        isToggleOn: true};
   }
 
   handleClick() {
     // this.setState({date: new Date()});
+    console.log('this is:', this);
     this.tick();
+    this.setState(state => ({
+        isToggleOn: !state.isToggleOn
+      }));
   }
 
   componentWillMount() {
@@ -65,6 +71,9 @@ class HelloTo extends React.Component {
       <div>
       <h2>Hello, {this.props.gender} {this.props.name}</h2>
       <h2 onClick={this.handleClick}>It is {this.state.date.toLocaleTimeString()}.</h2>
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
       </div>
     );
   }
