583c87ebb2a02df40d6756fa77f9c8838b374d8c
vincentliu99999 <vincentliu99999@gmail.com>
2019-01-25 17:02:38 +0800

    add form example

--- src/index.js
+++ src/index.js
@@ -5,13 +5,8 @@ import App from "./App";
 import HelloTo from "./HelloTo";
 import AlarmClockControl from "./AlarmClockControl";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Form from "./Form";
 import * as serviceWorker from "./serviceWorker";
-
-const Repos = () => (
-  <div>
-    <h2>Repos</h2>
-  </div>
-);
 
 const About = () => (
   <div>
@@ -49,20 +44,20 @@ ReactDOM.render(
       <div>
         <ul>
           <li><Link to="/">App</Link></li>
-          <li><Link to="/repos">Repos</Link></li>
           <li><Link to="/about">About</Link></li>
           <li>
             <Link to={{
-              pathname: '/hello',
-              state: { id: '1', message: 'hello, component' }
+                pathname: '/hello',
+                state: { id: '1', message: 'hello, component' }
             }}>Welcome</Link>
           </li>
+          <li><Link to="/form">Form</Link></li>
         </ul>
         <hr/>
         <Route exact path="/" component={App}/>
-        <Route path="/repos" component={Repos}/>
         <Route path="/about" component={About}/>
         <Route path="/hello" render={props =><HelloTo name="joe" gender="MR." {...props} />} />
+        <Route path="/form" component={Form}/>
         <NumberList numbers={numbers} />
       </div>
     </Router>,
