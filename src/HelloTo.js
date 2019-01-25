16232c79a9bff31561259f7d91125ffad7c8b1e4
vincentliu99999 <vincentliu99999@gmail.com>
2019-01-25 10:11:30 +0800

    add HelloTO comonent

---
+++ src/HelloTo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class HelloTo extends React.Component {
+    render() {
+        console.dir(this.props);
+        return <h2>Hello, {this.props.name}</h2>
+    }
+}
+
+export default HelloTo;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HelloTo from './HelloTo'
 import * as serviceWorker from './serviceWorker';
 
-function HelloTo(props) {
-    return <h1>Hi, {props.name}</h1>;
-  }
-  
-  const people = {"name": "Vincent Liu"};
-
-ReactDOM.render(HelloTo(people), document.getElementById('root'));
+ReactDOM.render(<HelloTo name="Vincent" gender="Mr." />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
