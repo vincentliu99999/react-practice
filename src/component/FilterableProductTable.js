7ff4b646202db13e33d888d81e585e1766d1b4dd
vincentliu99999 <vincentliu99999@gmail.com>
2019-01-28 15:43:03 +0800

    add stock example 3

--- src/component/FilterableProductTable.js
+++ src/component/FilterableProductTable.js
@@ -77,20 +77,35 @@ class ProductCategoryRow extends React.Component {
   }
   
   class SearchBar extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+      this.handleInStockChange = this.handleInStockChange.bind(this);
+    }
+    
+    handleFilterTextChange(e) {
+      this.props.onFilterTextChange(e.target.value);
+    }
+    
+    handleInStockChange(e) {
+      this.props.onInStockChange(e.target.checked);
+    }
+    
     render() {
-      const filterText = this.props.filterText;
-      const inStockOnly = this.props.inStockOnly;
-  
       return (
         <form>
           <input
             type="text"
             placeholder="Search..."
-            value={filterText} />
+            value={this.props.filterText}
+            onChange={this.handleFilterTextChange}
+          />
           <p>
             <input
               type="checkbox"
-              checked={inStockOnly} />
+              checked={this.props.inStockOnly}
+              onChange={this.handleInStockChange}
+            />
             {' '}
             Only show products in stock
           </p>
@@ -106,6 +121,21 @@ class ProductCategoryRow extends React.Component {
         filterText: '',
         inStockOnly: false
       };
+      
+      this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+      this.handleInStockChange = this.handleInStockChange.bind(this);
+    }
+  
+    handleFilterTextChange(filterText) {
+      this.setState({
+        filterText: filterText
+      });
+    }
+    
+    handleInStockChange(inStockOnly) {
+      this.setState({
+        inStockOnly: inStockOnly
+      })
     }
   
     render() {
@@ -114,6 +144,8 @@ class ProductCategoryRow extends React.Component {
           <SearchBar
             filterText={this.state.filterText}
             inStockOnly={this.state.inStockOnly}
+            onFilterTextChange={this.handleFilterTextChange}
+            onInStockChange={this.handleInStockChange}
           />
           <ProductTable
             products={this.props.products}
