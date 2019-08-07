e5a3d44aca562903ff0da6fc4808bfac49320e71
vincentliu99999 <vincentliu99999@gmail.com>
2019-08-08 00:00:05 +0800

    add chaining modal

--- src/component/antd/AntdModal.js
+++ src/component/antd/AntdModal.js
@@ -5,40 +5,66 @@ import 'antd/dist/antd.css';
 
 class AntdModal extends Component {
   state = {
-    visible: false
+    visible1: false,
+    visible2: false,
   };
 
-  showModal = () => {
+  showModal1 = () => {
     this.setState({
-      visible: true,
+      visible1: true,
     });
   };
 
-  handleOk = (e) => {
+  handleOk1 = (e) => {
     console.log(e);
     this.setState({
-      visible: false,
+      visible1: false,
+      visible2: true,
     });
   };
 
-  handleCancel = (e) => {
+  handleCancel1 = (e) => {
     console.log(e);
     this.setState({
-      visible: false,
+      visible1: false,
+    });
+  };
+
+  handleOk2 = (e) => {
+    console.log(e);
+    this.setState({
+      visible2: false,
+    });
+  };
+
+  handleCancel12= (e) => {
+    console.log(e);
+    this.setState({
+      visible2: false,
     });
   };
 
   render() {
     return (
       <div>
-        <Button type="primary" onClick={this.showModal}>
-          Open Modal
+        <Button type="primary" onClick={this.showModal1}>
+          Open Modal 1
         </Button>
         <Modal
-          title="Basic Modal"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
+          title="Basic Modal 1"
+          visible={this.state.visible1}
+          onOk={this.handleOk1}
+          onCancel={this.handleCancel1}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+        <Modal
+          title="Basic Modal 2"
+          visible={this.state.visible2}
+          onOk={this.handleOk2}
+          onCancel={this.handleCancel2}
         >
           <p>Some contents...</p>
           <p>Some contents...</p>
