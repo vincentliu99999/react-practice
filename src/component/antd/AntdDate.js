3e8bd6355f0a7547b4ef9abfa205c735652db5d5
Vincent Liu <vincentliu99999@gmail.com>
2020-06-02 11:24:33 +0800

    set moment locale to zh-tw

--- src/component/antd/AntdDate.js
+++ src/component/antd/AntdDate.js
@@ -4,6 +4,7 @@ import { DatePicker } from 'antd';
 import 'antd/dist/antd.css';
 
 import moment from 'moment';
+import 'moment/locale/zh-tw';
 
 const { RangePicker } = DatePicker;
 
@@ -16,10 +17,13 @@ class AntdDate extends Component {
   handleDateChange = (date, dateString) => {
     console.log('handleDateChange');
     console.log(`
+locale: ${moment.locale()}
 date: ${date}
 dateString: ${dateString}
+moment.isMoment(obj): ${moment.isMoment(date)}
 ${moment().format('MMMM Do YYYY, h:mm:ss a')}
-${moment(date, 'YYYY-MM-DD hh:mm:ss').format('YYYY-MM-DD hh:mm:ss')}`);
+${moment(date, 'YYYY-MM-DD hh:mm:ss').format('YYYY-MM-DD hh:mm:ss')}
+${date.format('YYYY-MM-DD hh:mm:ss')}`);
   };
 
   handleRangeChange = (date, dateString) => {
