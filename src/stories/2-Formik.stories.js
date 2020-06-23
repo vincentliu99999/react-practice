59a0887451df55239e42a41c68e1c12f26ba5277
Vincent Liu <vincentliu99999@gmail.com>
2020-06-23 22:36:10 +0800

    add formik storybook

---
+++ src/stories/2-Formik.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from '../component/formik/Form';
+import Basic from '../component/formik/Basic';
+
+export default {
+  component: Basic,
+  title: 'Formik',
+};
+
+export const FormikBasic = () => <Basic />;
+
+export const FormikForm = () => <Form />;
