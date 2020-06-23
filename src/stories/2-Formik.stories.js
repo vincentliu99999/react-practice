dda442fdbdf8569105af34e0debf01025db321db
Vincent Liu <vincentliu99999@gmail.com>
2020-06-24 00:01:35 +0800

    add antd css

--- src/stories/2-Formik.stories.js
+++ src/stories/2-Formik.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from '../component/formik/Form';
 import Basic from '../component/formik/Basic';
+import 'antd/dist/antd.css';
 
 export default {
   component: Basic,
