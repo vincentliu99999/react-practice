b274b3a1ac3c5c487961ebcbf2c882b6f87cddc2
Vincent Liu <vincentliu99999@gmail.com>
2020-06-24 13:09:13 +0800

    add range

--- src/stories/Knobs.stories.js
+++ src/stories/Knobs.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  withKnobs, text, boolean, number
+  withKnobs, text, boolean, number,
 } from '@storybook/addon-knobs';
 
 export default {
@@ -10,17 +10,23 @@ export default {
 // Add the `withKnobs` decorator to add knobs support to your stories.
 // You can also configure `withKnobs` as a global decorator.
 
-// Knobs for React props
+// boolean, text
 export const withAButton = () => (
   <button type="button" disabled={boolean('Disabled', false)}>
     {text('Label', 'Hello Storybook')}
   </button>
 );
 
-// Knobs as dynamic variables.
+// text, number
 export const asDynamicVariables = () => {
   const name = text('Name', 'James');
-  const age = number('Age', 35);
+  const options = {
+    range: true,
+    min: 35,
+    max: 90,
+    step: 1,
+  };
+  const age = number('Age', 35, options);
   const content = `I am ${name} and I'm ${age} years old.`;
 
   return <div>{content}</div>;
