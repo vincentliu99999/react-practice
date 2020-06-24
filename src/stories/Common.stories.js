f13f9b175df240af6f2480fd970fccf32abba600
Vincent Liu <vincentliu99999@gmail.com>
2020-06-24 17:54:05 +0800

    remove sample

--- src/stories/Common.stories.js
+++ src/stories/Common.stories.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import {
-  withKnobs, text, boolean, number, array
+  withKnobs, array
 } from '@storybook/addon-knobs';
 import FilterableProductTable from '../components/common/FilterableProductTable';
 
 export default {
   title: 'Common',
   decorators: [withKnobs]
-};
-// Add the `withKnobs` decorator to add knobs support to your stories.
-// You can also configure `withKnobs` as a global decorator.
-
-// Knobs for React props
-export const withAButton = () => (
-  <button type="button" disabled={boolean('Disabled', false)}>
-    {text('Label', 'Hello Storybook')}
-  </button>
-);
-
-// Knobs as dynamic variables.
-export const asDynamicVariables = () => {
-  const name = text('Name', 'James');
-  const age = number('Age', 35);
-  const content = `I am ${name} and I'm ${age} years old.`;
-
-  return <div>{content}</div>;
 };
 
 const products = [
@@ -48,7 +30,7 @@ const products = [
   }
 ];
 
-export const table = () => {
+export const ProductTable = () => {
   const value = array('value', products);
   return <FilterableProductTable products={value} />;
 };
