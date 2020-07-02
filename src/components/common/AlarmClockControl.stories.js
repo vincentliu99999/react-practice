716de9206ffd5d06be4cf5dd3ba54f17739bd113
Vincent Liu <vincentliu99999@gmail.com>
2020-07-02 16:47:29 +0800

    move components

---
+++ src/components/common/AlarmClockControl.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AlarmClockControl from './AlarmClockControl';
+
+export default {
+  title: 'Common',
+  component: AlarmClockControl,
+};
+
+export const AlarmClockControlUI = () => {
+  return <AlarmClockControl />;
+};
--- src/components/common/FilterableProductTable.js
+++ src/components/common/FilterableProductTable.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ProductCategoryRow extends Component {
   render() {
@@ -163,5 +164,37 @@ class FilterableProductTable extends React.Component {
     );
   }
 }
+
+FilterableProductTable.defaultProps = {
+  products: [
+    {
+      category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'
+    },
+    {
+      category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'
+    },
+    {
+      category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'
+    },
+    {
+      category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'
+    },
+    {
+      category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'
+    },
+    {
+      category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'
+    }
+  ]
+};
+
+FilterableProductTable.propTypes = {
+  products: PropTypes.arrayOf(PropTypes.shape({
+    category: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    stocked: PropTypes.bool.isRequired,
+    name: PropTypes.string.isRequired,
+  })).isRequired,
+};
 
 export default FilterableProductTable;
