70f6a68b9c7663736014f4c50fe7323d6d4ec358
Vincent Liu <vincentliu99999@gmail.com>
2020-08-18 15:08:35 +0800

    add side effect

--- src/components/hook/Example.js
+++ src/components/hook/Example.js
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Example() {
   // 宣告一個新的 state 變數，我們叫他「count」
   const [count, setCount] = useState(0);
+  console.dir(useState(0));
+
+  // 與 componentDidMount 和 componentDidUpdate 類似：
+  useEffect(() => {
+    // 使用瀏覽器 API 更新文件標題
+    document.title = `You clicked ${count} times`;
+  });
 
   return (
     <div>
