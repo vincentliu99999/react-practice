00972d6df4d92ef564d16034ec2e40737d3faead
Vincent Liu <vincentliu99999@gmail.com>
2020-08-20 22:05:33 +0800

    test travis

--- scripts/env.sh
+++ scripts/env.sh
@@ -6,20 +6,26 @@ pwd
 # Remove .env
 rm -rf parameters.json
 
+echo $TRAVIS_BUILD_NUMBER
+
 case "$1" in
     master)
         echo generating master config
-        cp parameters-dev.json parameters.json
+        cp ./config/parameters-dev.json parameters.json
         ;;
     stg)
         echo generating stg config
-        cp parameters-stg.json parameters.json
+        cp ./config/parameters-stg.json parameters.json
         ;;
     prod)
         echo generating prod config
-        cp parameters-prod.json parameters.json
+        cp ./config/parameters-prod.json parameters.json
         ;;
-    *) echo "Environment string must be develop/staging/master string!" ; exit 1 ;;
+    travis)
+        echo travis test
+        touch parameters.json
+        ;;
+    *) echo "Environment string must be develop/staging/master/travis string!" ; exit 1 ;;
 esac
 
 cat parameters.json | xargs echo -e
